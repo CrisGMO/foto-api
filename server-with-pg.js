cb59a7f9153a127df191c5198d7c03fa4aa99738
CrisGMO <cristiancampos1508@gmail.com>
2024-05-20 23:31:56 -0300

    delete paquetes sin uso

--- server-with-pg.js
+++ server-with-pg.js
@@ -1,14 +1,4 @@
 import { createApp } from './app.js'
 import { FotoModel } from './Models/pg/fotos.js'
 
-import postgres from 'postgres'
-
-const sql = postgres({
-  host: process.env.DATABASE_HOST,
-  database: process.env.DATABASE_NAME,
-  username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  ssl: 'require',
-})
-
 createApp({ fotoModel: FotoModel })
