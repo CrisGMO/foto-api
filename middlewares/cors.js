8a925a2d27813cbb34a577497d8a108a385f7c81
CrisGMO <cristiancampos1508@gmail.com>
2025-04-03 14:13:45 -0300

    cors res

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -14,17 +14,39 @@ const ACCEPTED_ORIGINS = [
 ]
 
 // || origin.startsWith("http://localhost")
-export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
-  origin: (origin, callback) => {
-    console.log('Origin:', origin);
-    if (acceptedOrigins.includes(origin)) {
-      return callback(null, true)
+export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => 
+  (req, res, next) => {
+    const origin = req.headers.origin;
+
+    if (!origin || acceptedOrigins.includes(origin)) {
+      res.setHeader('Access-Control-Allow-Origin', origin || '*');
+      res.setHeader('Access-Control-Allow-Credentials', 'true'); // 🔹 Importante para credenciales
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+      // Si es una solicitud OPTIONS (preflight), responde de inmediato
+      if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+      }
+    } else {
+      return res.status(403).json({ message: `Not allowed by CORS: ${origin}` });
     }
 
-    if (!origin) {
-      return callback(null, true)
-    }
+    next();
+  };
 
-    return callback(new Error('Not allowed by CORS'))
-  }
-})
+
+// export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
+//   origin: (origin, callback) => {
+//     console.log('Origin:', origin);
+//     if (acceptedOrigins.includes(origin)) {
+//       return callback(null, true)
+//     }
+
+//     if (!origin) {
+//       return callback(null, true)
+//     }
+
+//     return callback(new Error('Not allowed by CORS'))
+//   }
+// })
